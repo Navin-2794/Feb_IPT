ab445f6f3cdbc79c821822416d8b4d5ca0bf8995
Saraaws <poongodi.singa@gmail.com>
2025-02-22 12:48:10 +0530

    issue resolved

--- src/main/java/com/FebIPT/App.java
+++ src/main/java/com/FebIPT/App.java
@@ -7,15 +7,15 @@ public class App {
     System.out.println("Hello World!");
   }
   
-<<<<<<< HEAD
+
   public void sara() {
 	  System.out.println("sara");
   }
-=======
+
   public void janani() {
 	  
 	  System.out.println("Completed");
 
 }
->>>>>>> ccf4a6aa9633e667c823456249e538ee64094117
+
 }
