2b7bec008038f586df7445f88bcbda03a41e0dcb
Vijayk3005 <vijayk3005@gmail.com>
2025-02-25 13:53:37 +0530

    vijay method created

--- src/main/java/com/FebIPT/App.java
+++ src/main/java/com/FebIPT/App.java
@@ -6,13 +6,8 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
-  
 
-  public void sara() {
-	  System.out.println("sara");
-  }
-
-  public void janani() {
+  public void vijay() {
 	  
 	  System.out.println("Completed");
 
